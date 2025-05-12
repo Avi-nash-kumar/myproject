cda77ddc72a085a0d5688a01a0ff3b5c7415fce8
Avinash Kumar <avipatel9196@gmail.com>
2025-05-12 17:11:09 +0530

    Initial Commit

---
+++ src/main/java/com/project/UserController.java
@@ -0,0 +1,33 @@
+package com.project;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	
+	@Autowired
+	private UserRepo userRepo;
+	
+	@PostMapping("/add")
+	public User saveUser(@RequestBody User user)
+	{
+		return userRepo.save(user);
+	}
+	
+	@GetMapping("/all")
+	public List<User> getAll()
+	{
+		return userRepo.findAll();
+	}
+	
+	
+
+}
---
+++ src/main/java/com/project/UserRepo.java
@@ -0,0 +1,9 @@
+package com.project;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long>{
+	
+	
+
+}
